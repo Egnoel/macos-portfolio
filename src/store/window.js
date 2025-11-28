de929e63865ce5da031e915c2706c4619d4e90b3
Egnoel <70013986+Egnoel@users.noreply.github.com>
2025-11-28 15:00:53 +0100

    implement hoc for managing window states and add the first window - terminal

--- src/store/window.js
+++ src/store/window.js
@@ -1,36 +1,39 @@
 import { INITIAL_Z_INDEX, WINDOW_CONFIG } from '#constants';
-import { Immer } from 'immer';
 import { create } from 'zustand';
+import { produce } from 'immer';
 
-const useWindowStore = () =>
-  create(
-    Immer((set) => ({
-      windows: WINDOW_CONFIG,
-      nextZIndex: INITIAL_Z_INDEX + 1,
+const useWindowStore = create((set) => ({
+  windows: WINDOW_CONFIG,
+  nextZIndex: INITIAL_Z_INDEX + 1,
 
-      openWindow: (windowKey, data = null) =>
-        set((state) => {
-          const win = state.windows[windowKey];
-          if (!win) return;
-          win.isOpen = true;
-          win.zIndex = state.nextZIndex++;
-          win.data = data ?? win.data;
-        }),
-      closeWindow: (windowKey) =>
-        set((state) => {
-          const win = state.windows[windowKey];
-          if (!win) return;
-          win.isOpen = false;
-          win.zIndex = INITIAL_Z_INDEX;
-          win.data = null;
-        }),
-      focusWindow: (windowKey) =>
-        set((state) => {
-          const win = state.windows[windowKey];
-          if (!win) return;
-          win.zIndex = state.nextZIndex++;
-        }),
-    }))
-  );
+  openWindow: (windowKey, data = null) =>
+    set(
+      produce((state) => {
+        const win = state.windows[windowKey];
+        if (!win) return;
+        win.isOpen = true;
+        win.zIndex = state.nextZIndex++;
+        win.data = data ?? win.data;
+      })
+    ),
+  closeWindow: (windowKey) =>
+    set(
+      produce((state) => {
+        const win = state.windows[windowKey];
+        if (!win) return;
+        win.isOpen = false;
+        win.zIndex = INITIAL_Z_INDEX;
+        win.data = null;
+      })
+    ),
+  focusWindow: (windowKey) =>
+    set(
+      produce((state) => {
+        const win = state.windows[windowKey];
+        if (!win) return;
+        win.zIndex = state.nextZIndex++;
+      })
+    ),
+}));
 
 export default useWindowStore;
